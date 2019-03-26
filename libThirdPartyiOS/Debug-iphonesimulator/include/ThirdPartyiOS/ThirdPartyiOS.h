b2d2e5bceccc1671a86e23e5fb32b412a01138b1
John Moshakis <john.moshakis@firmex.com>
2019-03-25 21:56:20 -0400

    latest

---
+++ libThirdPartyiOS/Debug-iphonesimulator/include/ThirdPartyiOS/ThirdPartyiOS.h
@@ -0,0 +1,13 @@
+//
+//  ThirdPartyiOS.h
+//  ThirdPartyiOS
+//
+//  Created by John Moshakis on 2015-03-15.
+//  Copyright (c) 2015 John Moshakis. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ThirdPartyiOS : NSObject
+
+@end
